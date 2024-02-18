f34e5be3d63ee96b66b61d1e9e49b0129e40c9d1
feng-Ergou <48033358+feng-Ergou@users.noreply.github.com>
2024-02-18 17:41:26 -0600

    lab_4

--- Lab_4/lab_4.py
+++ Lab_4/lab_4.py
@@ -1,19 +1,21 @@
 import arcpy
 
 #set environment workspace
-arcpy.env.workspace = r"F:\GEOG676\DevSource\Feng-GEOG676-spring2024\Lab_4"
+arcpy.env.workspace = r"F:/GEOG676/DevSource/Feng-GEOG676-spring2024/Lab_4/Workspace"
 path_gdb = str(arcpy.CreateFileGDB_management(arcpy.env.workspace, "lab_4.gdb"))
 
+#get buffer distance  from user
+bufferDis = input("Please enter the intend buffer distance (in meters): ") + " Meters"
 
 #add in data layers
 
 #add garages layer
-garages_o = arcpy.MakeXYEventLayer_management("Lab_4/garages.csv", "x", "y", "GAR_pt_o")
+garages_o = arcpy.MakeXYEventLayer_management("Lab_4/Inputs/garages.csv", "x", "y", "GAR_pt_o")
 arcpy.FeatureClassToGeodatabase_conversion(garages_o, path_gdb)
 layer_GAR_pt_o = path_gdb + "/GAR_pt_o"
 
 #add buildings layer
-layer_BLDG_o = "Lab_4/Campus.gdb" + "/Structures"
+layer_BLDG_o = "Lab_4/Inputs/Campus.gdb" + "/Structures"
 layer_BLDG = path_gdb + "/BLDG_ply"
 arcpy.Copy_management(layer_BLDG_o, layer_BLDG)
 
@@ -23,10 +25,11 @@ layer_GAR_pt = path_gdb + "/GAR_pt"
 arcpy.Project_management(layer_GAR_pt_o, layer_GAR_pt, ref_Spatial)
 
 #buffer garages
-layer_GAR_buffered = arcpy.Buffer_analysis(layer_GAR_pt, path_gdb + "/GAR_buf150", 150)
+name_GAR_buffered = "/GAR_buf" + bufferDis.split(" ")[0].replace(".","_")
+layer_GAR_buffered = arcpy.Buffer_analysis(layer_GAR_pt, path_gdb + name_GAR_buffered, bufferDis)
 
 #intersect buffered garages with buildings
 arcpy.Intersect_analysis([layer_GAR_buffered, layer_BLDG], path_gdb + "/GAR_BLDG_int", "ALL")
 
 #output to table
-arcpy.TableToTable_conversion(path_gdb + "/GAR_BLDG_int.dbf", "Lab_4", "nearbyBuildings.csv")
+arcpy.TableToTable_conversion(path_gdb + "/GAR_BLDG_int.dbf", "Lab_4/Outputs", "nearbyBuildings.csv")
